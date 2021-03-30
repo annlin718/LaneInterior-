89eef965be2d42694833f9b5bf930ff5bf145e74
annlin718 <benny800718@gmail.com>
2021-03-31 00:50:05 +0800

    20210321_v5

--- JS/main.js
+++ JS/main.js
@@ -17,7 +17,7 @@ window.onload = function () {
 
     }
     // $(".picLogo").attr("src", "../Sourse/logo1.png");
-    $(".picLogo").attr("src", "../Sourse/logo1.png");
+    $(".picLogo").attr("src", "/Sourse/logo1.png");
 };
 
 function goLink(sURL) {
